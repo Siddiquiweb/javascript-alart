4be4a5e12d60cba21d84b006a66652cb0bd4e4e2
Abdulrehman Siddiqui <157453608+Siddiquiweb@users.noreply.github.com>
2024-03-01 04:00:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+alert('wellcom vistors');
+console .log(123);
+alert ("Enter your name!");
+
+var userName = prompt ("please enter your name");
+var x = prompt ("how old are you?");
+alert ('Thankyou ' + confirm);
